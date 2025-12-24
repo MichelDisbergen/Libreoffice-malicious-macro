cff31b89edb8a82d802616782474a6418dfdb4d6
Corrosie <michel-disbergen@hotmail.com>
2025-12-24 11:39:04 +0100

    Fix usage message for local host and port

--- mmg-odp.py
+++ mmg-odp.py
@@ -7,7 +7,7 @@ init()
 def Help():
 
 	print ('LibreOffice/OpenOffice .odp Impress Document Malicious Macro Generator')
-	print ('Usage python3 mmg-odp.py <linux/windows> <rhost> <rport>')
+	print ('Usage python3 mmg-odp.py <linux/windows> <lhost> <lport>')
 	sys.exit(1)
 
 if len(sys.argv) < 4:
