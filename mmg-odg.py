4b0cccbb872c9de39389813042f0eb7f4dadb170
Corrosie <michel-disbergen@hotmail.com>
2025-12-24 11:38:52 +0100

    Update mmg-odg.py

--- mmg-odg.py
+++ mmg-odg.py
@@ -7,7 +7,7 @@ init()
 def Help():
 
 	print ('LibreOffice/OpenOffice .odg Draw Document Malicious Macro Generator')
-	print ('Usage python3 mmg-odg.py <linux/windows> <rhost> <rport>')
+	print ('Usage python3 mmg-odg.py <linux/windows> <lhost> <lport>')
 	sys.exit(1)
 
 if len(sys.argv) < 4:
